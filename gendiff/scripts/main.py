e05155249789ed082d147a0ede17ca10c467aacb
Maria Antipova <experiment0@yandex.ru>
2025-11-01 00:32:23 +0300

    Добавлены тестовые данные

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
+import json
+
 
 # uv run python ./gendiff/scripts/main.py -h
+# uv run python ./gendiff/scripts/main.py f1 f2
 def main():
     # Создаем объект парсера аргументов и указываем описание скрипта
     parser = ArgumentParser(description="Compares two configuration files and shows a difference.")
@@ -18,6 +21,11 @@ def main():
     args = parser.parse_args()
     # print(type(args)) # argparse.Namespace
     # print(args) # Namespace(first_file='f1', second_file='f2')
+    
+    file1_content = json.load(open("./tests/test_data/file1.json"))
+    # print(type(file1_content)) # dict
+    # print(file1_content) # {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    file2_content = json.load(open("./tests/test_data/file2.json"))
     
 
 if __name__ == "__main__":
