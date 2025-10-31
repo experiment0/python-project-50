dbe3e25c6a5197e63b05fd73f7ec7ed5c931f9f5
Maria Antipova <experiment0@yandex.ru>
2025-10-31 23:23:02 +0300

    Добавлен параметр format

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,16 +1,21 @@
 from argparse import ArgumentParser
 
-
+# uv run python ./gendiff/scripts/main.py -h
 def main():
     # Создаем объект парсера аргументов и указываем описание скрипта
     parser = ArgumentParser(description="Compares two configuration files and shows a difference.")
+    
+    # Добавляем именованный аргумент
+    parser.add_argument("-f", "--format", type=str, help="set format of output")
     
     # Добавляем 2 позиционных аргумента
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
     
+    #parser.print_help()
+    
     # Получаем результат разбора аргументов командной строки, с которыми был вызван скрипт    
-    # args = parser.parse_args()
+    args = parser.parse_args()
     # print(type(args)) # argparse.Namespace
     # print(args) # Namespace(first_file='f1', second_file='f2')
     
