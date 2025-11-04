ec948a8fc08a68ebedcf6aa8ebd01b5584ff7a4b
Maria Antipova <experiment0@yandex.ru>
2025-11-04 04:11:19 +0300

    Добавлен формат отображения plain

---
+++ gendiff/views/plain/utils.py
@@ -0,0 +1,15 @@
+from gendiff.models.calc_diff import DictValueType
+
+
+def dict_value_to_str(value: DictValueType) -> str:
+    if value is None:
+        return "null"
+    if value is True:
+        return "true"
+    if value is False:
+        return "false"
+    if isinstance(value, str):
+        return f"'{value}'"
+    if isinstance(value, (int, float)):
+        return str(value)
+    return "[complex value]"
--- gendiff/views/plain/view.py
+++ gendiff/views/plain/view.py
@@ -1,5 +1,40 @@
+from typing import List
+
 from gendiff.models.calc_diff import DiffsType
+from gendiff.views.plain.utils import dict_value_to_str
+    
 
-
-def get_plain_view(diffs: DiffsType) -> str:
-    return "plain view"
+def get_plain_view(diffs: DiffsType, property_path: List[str] = []) -> str:
+    diffs_lines = []
+    
+    for d in diffs:
+        current_property_path = property_path + [d.key]
+        property_name = ".".join(current_property_path)
+        
+        match d.result:
+            case "equal":
+                if d.children is not None:
+                    children_view = get_plain_view(
+                        d.children,
+                        current_property_path,
+                    )
+                    diffs_lines.append(children_view)
+            case "add":
+                new_value_str = dict_value_to_str(d.new_value)
+                diffs_lines.append(
+                    f"Property '{property_name}' was added " + 
+                    f"with value: {new_value_str}"
+                )
+            case "remove":
+                diffs_lines.append(
+                    f"Property '{property_name}' was removed"
+                )
+            case "change":
+                old_value_str = dict_value_to_str(d.old_value)
+                new_value_str = dict_value_to_str(d.new_value)
+                diffs_lines.append(
+                    f"Property '{property_name}' was updated. " + 
+                    f"From {old_value_str} to {new_value_str}"
+                )
+    
+    return "\n".join(diffs_lines)
